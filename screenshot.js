2d1a2c7b9734b446031ad23edde7897f1d163f17
Roel Vaneyghen <roel.vaneyghen@gmail.com>
2020-08-21 16:21:03 +1200

    bump packages

--- screenshot.js
+++ screenshot.js
@@ -1,15 +1,15 @@
-const chrome = require('chrome-aws-lambda');
+const chromium = require('chrome-aws-lambda');
 const puppeteer = require('puppeteer-core');
 
 async function getScreenshot(url, type = 'jpeg', quality = 80) {
   const browser = await puppeteer.launch({
-    args: chrome.args,
-    executablePath: await chrome.executablePath,
-    headless: chrome.headless,
+    args: chromium.args,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
   });
 
   const page = await browser.newPage();
-  await page.goto(`https://${url}`, { waitUntil: 'networkidle0' });
+  await page.goto(url, { waitUntil: 'networkidle0' });
   const file = await page.screenshot({ type, quality });
   await browser.close();
   return file;
